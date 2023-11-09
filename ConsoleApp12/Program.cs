0c92fd84e67f2cf9e3cced0cbd3285debba68799
Антон <@DESKTOP-CPLDV61>
2023-11-09 16:28:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static Mutex mutex = new Mutex(); // М'ютекс для синхронізації потоків
+    static bool firstThreadDone = false;
+
+    static void Main()
+    {
+        Thread firstThread = new Thread(FirstThreadMethod);
+        Thread secondThread = new Thread(SecondThreadMethod);
+
+        firstThread.Start();
+        secondThread.Start();
+
+        firstThread.Join();
+        secondThread.Join();
+    }
+
+    static void FirstThreadMethod()
+    {
+        mutex.WaitOne();
+
+        try
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine($"Thread 1: {i}");
+                Thread.Sleep(200); // Додайте затримку для емуляції реальної роботи
+            }
+
+            firstThreadDone = true;
+        }
+        finally
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+
+    static void SecondThreadMethod()
+    {
+        while (!firstThreadDone)
+        {
+            // Очікуємо завершення першого потоку
+            Thread.Sleep(100);
+        }
+
+        mutex.WaitOne();
+
+        try
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine($"Thread 2: {i}");
+                Thread.Sleep(200); // Додайте затримку для емуляції реальної роботи
+            }
+        }
+        finally
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+}
